97bd86a6126f15e1ceab5ea908c4c915cb5b7645
arjun1Fa <culerarjun@gmail.com>
2026-09-15 23:50:03 +0530

    Refactor two sum algorithm to correctly iterate through all pairs of numbers

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,11 +1,9 @@
 nums = [10 , 20 , 30 , 40 ,  50 ]
-target = 40
-i=0
+target = 50
 
 
-
-for i in range ((len(nums)) - 1):
-    j=i+1
-    if (nums[i]+nums[j]) ==target:
-        print("Target has been found")
+for k in range ((len(nums))):
+    for i in range (k+1,(len(nums))):
+        if (nums[k]+nums[i]) ==target:
+                print("Target has been found")
 
